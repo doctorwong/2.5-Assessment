04b22cbc4b03c963f4c5c944d687a9f1d4cb6774
Wilson Wong <williewonga@gmail.com>
2018-01-19 18:17:23 -0600

    Add files via upload

--- nightwatch/tests/test.js
+++ nightwatch/tests/test.js
@@ -1,4 +1,5 @@
 const selectors = require('../resource/selectors')
+const data = require('../resource/data')
 
 module.exports = {
     beforeEach: browser => {
@@ -13,34 +14,34 @@ module.exports = {
     //enters a series of numbers, separated by commas, and verifies the results are displayed in the appropriate fields
     checkEvensAndOdds: browser => {
         //enters numbers, separated by a comma, into the 'evenOddInput' field
-        browser.setValue(selectors.evenOddInput, '0,2,3,11,22,100,101');
+        browser.setValue(selectors.evenOddInput, data.inputs.evenOddInput);
         //clicks the submit button
         browser.click(selectors.evenOddButton);
         //verifies results in the evens box
-        browser.expect.element(selectors.evenResults).text.to.contain("[0,2,22,100]")
+        browser.expect.element(selectors.evenResults).text.to.contain(data.outputs.evenResults);
         //verifies results in the odds box
-        browser.expect.element(selectors.oddResults).text.to.contain("[3,11,101]")
+        browser.expect.element(selectors.oddResults).text.to.contain(data.outputs.oddResults);
     },
 
 //filters objects by key name
     checkFilterObject: browser => {
 
         //verifies all values for the key 'name'
-        browser.setValue(selectors.objectFilterInput, 'name');
+        browser.setValue(selectors.objectFilterInput, data.inputs.objectFilterInput[0]);
         browser.click(selectors.objectFilterButton);
-        browser.expect.element(selectors.objectFilterResults).text.to.contain(" [ { \"name\": \"Jimmy Joe\", \"title\": \"Hack0r\", \"age\": 12 }, { \"name\": \"Jeremy Schrader\", \"age\": 24, \"hairColor\": \"brown\" }, { \"name\": \"Carly Armstrong\", \"title\": \"CEO\" } ]")
+        browser.expect.element(selectors.objectFilterResults).text.to.contain(data.outputs.objectFilterResults[0]);
         
         //verifies all values for the key 'title'
         browser.clearValue(selectors.objectFilterInput);
-        browser.setValue(selectors.objectFilterInput, 'title');
+        browser.setValue(selectors.objectFilterInput, data.inputs.objectFilterInput[1]);
         browser.click(selectors.objectFilterButton);
-        browser.expect.element(selectors.objectFilterResults).text.to.contain(" [ { \"name\": \"Jimmy Joe\", \"title\": \"Hack0r\", \"age\": 12 }, { \"name\": \"Carly Armstrong\", \"title\": \"CEO\" } ]")
+        browser.expect.element(selectors.objectFilterResults).text.to.contain(data.outputs.objectFilterResults[1]);
 
         //searches for an invalid key. should contain no results
         browser.clearValue(selectors.objectFilterInput);
-        browser.setValue(selectors.objectFilterInput, 'empty');
+        browser.setValue(selectors.objectFilterInput, data.inputs.objectFilterInput[2]);
         browser.click(selectors.objectFilterButton);
-        browser.expect.element(selectors.objectFilterResults).text.to.contain("Filtered: []")
+        browser.expect.element(selectors.objectFilterResults).text.to.contain(data.outputs.objectFilterResults[2]);
 
     },
     
@@ -48,15 +49,15 @@ module.exports = {
     checkFilterString: browser => {
 
         //displays names containing 'J'
-        browser.setValue(selectors.nameFilterInput, 'J');
+        browser.setValue(selectors.nameFilterInput, data.inputs.nameFilterInput[0]);
         browser.click(selectors.nameFilterButton);
-        browser.expect.element(selectors.nameFilterResults).text.to.contain(" [ \"James\", \"Jessica\", \"Jennifer\" ]")
+        browser.expect.element(selectors.nameFilterResults).text.to.contain(data.outputs.nameFilterResults[0]);
         
         //submits a search that should contain no results
         browser.clearValue(selectors.nameFilterInput);
-        browser.setValue(selectors.nameFilterInput, 'empty');
+        browser.setValue(selectors.nameFilterInput, data.inputs.nameFilterInput[1]);
         browser.click(selectors.nameFilterButton);
-        browser.expect.element(selectors.nameFilterResults).text.to.contain("Filtered Names: []")
+        browser.expect.element(selectors.nameFilterResults).text.to.contain(data.outputs.nameFilterResults[1]);
      },
 
     //submits an input and checks if it is a palindrome
@@ -64,29 +65,29 @@ module.exports = {
     checkPalindrome: browser => {
 
         //checks a palindrome input
-        browser.setValue(selectors.palindromeInput, 'abcba');
+        browser.setValue(selectors.palindromeInput, data.inputs.palindromeInput[0]);
         browser.click(selectors.palindromeButton);
-        browser.expect.element(selectors.palindromeResults).text.to.contain("true")
+        browser.expect.element(selectors.palindromeResults).text.to.contain(data.outputs.palindromeResults[0]);
         //checks a nonpalindrome input
         browser.clearValue(selectors.palindromeInput);
-        browser.setValue(selectors.palindromeInput, 'abbcc');
+        browser.setValue(selectors.palindromeInput, data.inputs.palindromeInput[1]);
         browser.click(selectors.palindromeButton);
-        browser.expect.element(selectors.palindromeResults).text.to.contain("false")
+        browser.expect.element(selectors.palindromeResults).text.to.contain(data.outputs.palindromeResults[1]);
 
         //checks no input
         browser.clearValue(selectors.palindromeInput);
         browser.click(selectors.palindromeButton);
-        browser.expect.element(selectors.palindromeResults).text.to.contain("false")
+        browser.expect.element(selectors.palindromeResults).text.to.contain(data.outputs.palindromeResults[2]);
 
     },
 
     //checks if the sum of two numbers is correct
     checkSum: browser => {
 
-        browser.setValue(selectors.sumInput1, '5');
-        browser.setValue(selectors.sumInput2, '5');
+        browser.setValue(selectors.sumInput1, data.inputs.sumInput1);
+        browser.setValue(selectors.sumInput2, data.inputs.sumInput2);
         browser.click(selectors.sumButton);
-        browser.expect.element(selectors.sumResults).text.to.contain("10")
+        browser.expect.element(selectors.sumResults).text.to.contain(data.outputs.sumResults);
     }
 
 }
